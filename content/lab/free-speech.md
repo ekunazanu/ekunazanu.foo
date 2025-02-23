55a79dfd800c641d40f7e032c9849256af99f49e
ekunazanu <me@ekunazanu.foo>
2025-02-24 01:11:27 +0530

    content: add lab post #3

---
+++ content/lab/free-speech.md
@@ -0,0 +1,28 @@
++++
+title = "Free Speech"
+description = "Visualising frequently used word submissions."
+weight = 8
+draft = false
+template = "article.html"
+[extra]
+type = "article"
+thumbnail = "lab.words.svg"
+thumbnailalt = "A messy word-cloud filled with random words."
++++
+
+This is another experimental post. Arguably more 'experimental' than the previous ones.
+
+<canvas id="canvasTop"></canvas>
+<button id="buttonRefreshTop">Refresh</button>
+
+This is simply the list of all the words submitted here. Everything gets added; nothing is censored. While this project did not feel like a novel idea, I could not find anything similar surprisingly.
+
+Anyway, you can add to the dataset too.
+
+<input id="inputWords" placeholder="Add any word"><button id="buttonAdd">Add</button><button id="buttonRefreshDist">Refresh</button>
+<canvas id="canvasDistribution"></canvas>
+<div id="infoDistribution"></div>
+
+The entire database is only a single mebibyte big and so it uses a few [nice tricks](/log/4-pb-mb-db) to store the data. This however means the data is 'only' around 98% accurate.
+
+<script src="/scripts/free-speech.js"></script>
---
+++ static/scripts/free-speech.js
@@ -0,0 +1,161 @@
+// code is all over the place
+// it can be refactored to be DRY and not use global variables
+// but i am on a time crunch
+
+const canvasTop = document.getElementById("canvasTop").getContext("2d");
+const canvasDistribution = document.getElementById("canvasDistribution").getContext("2d");
+const buttonRefreshTop = document.getElementById("buttonRefreshTop");
+const buttonRefreshDist = document.getElementById("buttonRefreshDist");
+const buttonAdd = document.getElementById("buttonAdd");
+const inputWords = document.getElementById("inputWords");
+const infoDistribution = document.getElementById("infoDistribution");
+
+canvasTop.canvas.width = 1280;
+canvasTop.canvas.height = 350;
+canvasDistribution.canvas.width = 1280;
+canvasDistribution.canvas.height = 650;
+
+var topItems = {};
+var topCounts = [];
+var topElements = [];
+var countDistribution = {};
+var currentItem = {rank: 1, count: 0, rawRank: 0};
+
+getDistribution("https://words.ekunazanu.foo/dist");
+syncChart("https://words.ekunazanu.foo", 16);
+
+buttonRefreshTop.addEventListener('click', async function() {
+    syncChart("https://words.ekunazanu.foo", 16);
+});
+
+buttonRefreshDist.addEventListener('click', async function() {
+    getDistribution("https://words.ekunazanu.foo/dist");
+});
+
+buttonAdd.addEventListener('click', async function() {
+    var inputData = inputWords.value
+    if (inputData == "") return;
+    postWord(inputWords.value, "https://words.ekunazanu.foo/item");
+});
+
+async function syncChart(url, n = 16) {
+    topItems = await getTop(url);
+    sortCounts(topItems, n);
+    getArrays(topItems, topCounts, topElements, n);
+    drawTop(canvasTop, topCounts, topElements, 50, 70);
+}
+
+async function getTop(url) {
+    try {
+        var response = await fetch(url);
+        var itemsObject = await response.json();
+        return itemsObject
+    } catch (error) {
+        canvasTop.canvas.style.display = "none";
+        buttonRefreshTop.style.display = "none";
+    }
+}
+
+async function getDistribution(url) {
+    try {
+        countDistribution = await getTop(url);
+        countDistribution.distribution.sort( function(a, b) { return a - b; }).reverse();
+        drawDistribution(canvasDistribution, countDistribution.distribution, countDistribution.cardinality);
+    } catch (error) { handleServerDisconnect(); }
+}
+
+async function postWord(word, url) {
+    await fetch(url, {
+        method: "POST",
+        headers: {"Content-Type": "text/plain"},
+        body: word
+    })
+    .then(response => response.json())
+    .then(data => {
+        currentItem.count = data;
+        getRank(data);
+        drawDistribution(canvasDistribution, countDistribution.distribution, countDistribution.cardinality);
+        canvasDistribution.fillStyle = "#000";
+        canvasDistribution.fillRect(currentItem.rawRank * 5, 600 - currentItem.count * 600 / countDistribution.distribution[0], 5, currentItem.count * 600 / countDistribution.distribution[0]);
+        infoDistribution.innerHTML = `Added '${word}'. It appears roughly ${data} times, at approximately the ${Math.round(currentItem.rank / 255 * 100)}th percentile, with a rank of about ${Math.round(currentItem.rank / 255 * countDistribution.cardinality + 1)}.`;
+    })
+    .catch(error => { handleServerDisconnect(); });
+}
+
+function handleServerDisconnect() {
+    inputWords.style.display = "none";
+    buttonAdd.style.display = "none";
+    buttonRefreshDist.style.display = "none";
+    canvasDistribution.canvas.style.display = "none";
+    infoDistribution.innerHTML = 'Nevermind, you cannot. See <a href="/log/6-back-on-track">why</a>.'
+}
+
+function sortCounts(itemsObject, n = 16) {
+    var temp = itemsObject.top16[0];
+    for (let i = n - 1; i > 0; i--) {
+        let maximum = 0;
+        for (let j = 1; j <= i; j++) {
+            if (itemsObject.top16[j].count > itemsObject.top16[maximum].count) {
+                maximum = j;
+            }
+        }
+        temp = itemsObject.top16[maximum];
+        itemsObject.top16[maximum] = itemsObject.top16[i];
+        itemsObject.top16[i] = temp;
+    }
+}
+
+function getArrays(itemsObject, countArray, elementArray, n = 16) {
+    for (let i = 0; i < n; i++) {
+        countArray[i] = itemsObject.top16[n - 1 - i].count
+        var byteVal = itemsObject.top16[n - 1 - i].element
+        var strgVal = new TextDecoder().decode(Uint8Array.from(byteVal));
+        elementArray[i] = strgVal
+    }
+}
+
+function getRank(count, n = 256) {
+    var rank = 0;
+    var zeroes = 0;
+    for (let i = 0; i < n; i++) {
+        if (count < countDistribution.distribution[i]) { rank++ }
+        if (countDistribution.distribution[i] == 0) { zeroes++ }
+    }
+    currentItem.rawRank = rank;
+    currentItem.rank = rank * 255 / (256 - zeroes)
+}
+
+function drawTop(canvas, frequencies, words) {
+    var normalizedFrequencies = getNormalizedFrequencies(frequencies, 1280);
+    canvas.clearRect(0, 0, 1280, 350);
+    canvas.fillStyle = "#aaa";
+    for (let i = 0; i < frequencies.length; i++)
+        canvas.fillRect(0, i * 70 + 10, normalizedFrequencies[i], 50);
+    canvas.textAlign = "left";
+    canvas.textBaseline = "middle";
+    canvas.fillStyle = "#000";
+    canvas.font = "28px Inter";
+    for (let i = 0; i < words.length; i++)
+        canvas.fillText(frequencies[i] + " → " + words[i], 20, i * 70 + 38);
+}
+
+function drawDistribution(canvas, frequencies, cardinality) {
+    canvas.clearRect(0, 0, 1280, 650);
+    var normalizedFrequencies = getNormalizedFrequencies(frequencies, 600);
+    canvas.fillStyle = "#aaa";
+    for (let i = 0; i < frequencies.length; i++)
+        canvas.fillRect(i * 5, 600 - normalizedFrequencies[i], 5, normalizedFrequencies[i])
+    canvas.textAlign = "center";
+    canvas.textBaseline = "middle";
+    canvas.fillStyle = "#000";
+    canvas.font = "28px Inter";
+    canvas.fillText("← Approximately "+ cardinality + " total elements →", 640, 625);
+}
+
+function getNormalizedFrequencies(frequencies, maxValue = 1280) {
+    let norm = maxValue / frequencies[0];
+    let newFrequencies = [];
+    for (let i = 0; i < frequencies.length; i++)
+        newFrequencies.push(Math.floor(frequencies[i] * norm));
+    return newFrequencies;
+}
