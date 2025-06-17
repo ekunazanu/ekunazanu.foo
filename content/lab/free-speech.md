4c0c220892b8d7fbca93043c402b08b6580396d3
ekunazanu <me@ekunazanu.foo>
2025-06-18 02:52:30 +0530

    fix: content/lab/#3: use svg instead of canvas
    The chart does not need to be redrawn constantly and is relatively simple, so SVGs are more appropriate, since they will allow selecting text and are much sharper.

--- content/lab/free-speech.md
+++ content/lab/free-speech.md
@@ -12,7 +12,7 @@ thumbnailalt = "A messy word-cloud filled with random words."
 
 This is another experimental post. Arguably more 'experimental' than the previous ones.
 
-<canvas id="canvasTop"></canvas>
+<div id="svgContainerTop"></div>
 <button id="buttonRefreshTop">Refresh</button>
 
 This is simply the list of all the words submitted here. Everything gets added; nothing is censored. While this project did not feel like a novel idea, I could not find anything similar surprisingly.
@@ -20,7 +20,7 @@ This is simply the list of all the words submitted here. Everything gets added; 
 Anyway, you can add to the dataset too.
 
 <input id="inputWords" placeholder="Add any word"><button id="buttonAdd">Add</button><button id="buttonRefreshDist">Refresh</button>
-<canvas id="canvasDistribution"></canvas>
+<div id="svgContainerDistribution"></div>
 <div id="infoDistribution"></div>
 
 The entire database is only a single mebibyte big and so it uses a few [nice tricks](/log/4-pb-mb-db) to store the data. This however means the data is 'only' around 98% accurate.
--- static/scripts/free-speech.js
+++ static/scripts/free-speech.js
@@ -2,18 +2,16 @@
 // it can be refactored to be DRY and not use global variables
 // but i am on a time crunch
 
-const canvasTop = document.getElementById("canvasTop").getContext("2d");
-const canvasDistribution = document.getElementById("canvasDistribution").getContext("2d");
+const svgContainerTop = document.getElementById("svgContainerTop")
+const svgContainerDistribution = document.getElementById("svgContainerDistribution")
 const buttonRefreshTop = document.getElementById("buttonRefreshTop");
 const buttonRefreshDist = document.getElementById("buttonRefreshDist");
 const buttonAdd = document.getElementById("buttonAdd");
 const inputWords = document.getElementById("inputWords");
 const infoDistribution = document.getElementById("infoDistribution");
 
-canvasTop.canvas.width = 1280;
-canvasTop.canvas.height = 0;
-canvasDistribution.canvas.width = 1280;
-canvasDistribution.canvas.height = 0;
+const svgTop = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+const svgDistribution = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
 var topItems = {};
 var topCounts = [];
@@ -42,8 +40,7 @@ async function syncChart(url, n = 16) {
     topItems = await getTop(url);
     sortCounts(topItems, n);
     getArrays(topItems, topCounts, topElements, n);
-    canvasTop.canvas.height = 1050;
-    drawTop(canvasTop, topCounts, topElements, 50, 70);
+    drawTop(svgTop, svgContainerTop, topCounts, topElements, 50, 70);
 }
 
 async function getTop(url) {
@@ -52,7 +49,6 @@ async function getTop(url) {
         var itemsObject = await response.json();
         return itemsObject
     } catch (error) {
-        canvasTop.canvas.style.display = "none";
         buttonRefreshTop.style.display = "none";
     }
 }
@@ -61,8 +57,7 @@ async function getDistribution(url) {
     try {
         countDistribution = await getTop(url);
         countDistribution.distribution.sort( function(a, b) { return a - b; }).reverse();
-        canvasDistribution.canvas.height = 650;
-        drawDistribution(canvasDistribution, countDistribution.distribution, countDistribution.cardinality);
+        drawDistribution(svgDistribution, svgContainerDistribution, countDistribution.distribution, countDistribution.cardinality);
     } catch (error) { handleServerDisconnect(); }
 }
 
@@ -76,11 +71,9 @@ async function postWord(word, url) {
     .then(data => {
         currentItem.count = data;
         getRank(data);
-        drawDistribution(canvasDistribution, countDistribution.distribution, countDistribution.cardinality);
-        canvasDistribution.fillStyle = "#fe8";
-        canvasDistribution.fillRect(currentItem.rawRank * 5, 0, 5, 600);
-        canvasDistribution.fillStyle = "#000";
-        canvasDistribution.fillRect(currentItem.rawRank * 5, 600 - currentItem.count * 600 / countDistribution.distribution[0], 5, currentItem.count * 600 / countDistribution.distribution[0]);
+        drawDistribution(svgDistribution, svgContainerDistribution, countDistribution.distribution, countDistribution.cardinality);
+        createRectangle(svgDistribution, currentItem.rawRank * 5, 0, 5, 600, null, "#fe8");
+        createRectangle(svgDistribution, currentItem.rawRank * 5, 600 - currentItem.count * 600 / countDistribution.distribution[0], 5, currentItem.count * 600 / countDistribution.distribution[0], null, "#000");
         infoDistribution.innerHTML = `Added <mark>${word}</mark>. It appears roughly <strong>${data} times</strong>, at about the <strong>${Math.round(currentItem.rank / 255 * 100)}th percentile</strong>, with an approximate <strong>rank of ${Math.round(currentItem.rank / 255 * countDistribution.cardinality + 1)}</strong>.`;
     })
     .catch(error => { handleServerDisconnect(); });
@@ -90,7 +83,6 @@ function handleServerDisconnect() {
     inputWords.style.display = "none";
     buttonAdd.style.display = "none";
     buttonRefreshDist.style.display = "none";
-    canvasDistribution.canvas.style.display = "none";
     infoDistribution.innerHTML = 'Nevermind, you cannot. See <a href="/log/6-back-on-track">why</a>.'
 }
 
@@ -129,31 +121,56 @@ function getRank(count, n = 256) {
     currentItem.rank = rank * 255 / (256 - zeroes)
 }
 
-function drawTop(canvas, frequencies, words) {
+function drawTop(svg, container, frequencies, words) {
+    svg.innerHTML = "";
+    svg.setAttribute("width", 1280);
+    svg.setAttribute("height", 1050);
+    svg.setAttribute("viewBox", "0 0 1280 1050");
     var normalizedFrequencies = getNormalizedFrequencies(frequencies, 1280);
-    canvas.clearRect(0, 0, 1280, 1050);
-    canvas.fillStyle = "#aaa";
-    for (let i = 0; i < frequencies.length; i++)
-        canvas.fillRect(0, i * 70 + 10, normalizedFrequencies[i], 50);
-    canvas.textAlign = "left";
-    canvas.textBaseline = "middle";
-    canvas.fillStyle = "#000";
-    canvas.font = "28px Inter";
     for (let i = 0; i < words.length; i++)
-        canvas.fillText(frequencies[i] + " → " + words[i], 20, i * 70 + 38);
+        createRectangle(svg, 0, i * 70 + 10, normalizedFrequencies[i], 50, frequencies[i] + " → " + words[i]);
+    container.appendChild(svg);
 }
 
-function drawDistribution(canvas, frequencies, cardinality) {
-    canvas.clearRect(0, 0, 1280, 650);
+function createRectangle(svg, x, y, width, height, text = null, color = "#aaa") {
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    const txt = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    rect.setAttribute("x", x);
+    rect.setAttribute("y", y);
+    rect.setAttribute("width", width);
+    rect.setAttribute("height", height);
+    rect.setAttribute("fill", color);
+    svg.appendChild(rect);
+    if (text) {
+        txt.setAttribute("x", 20);
+        txt.setAttribute("y", y + height / 2 + 10);
+        txt.setAttribute("alignment-baseline", "middle");
+        txt.setAttribute("text-anchor", "start");
+        txt.setAttribute("font-size", "28");
+        txt.setAttribute("fill", "#000");
+        txt.textContent = text;
+        svg.appendChild(txt);
+    }
+}
+
+function drawDistribution(svg, container, frequencies, cardinality) {
+    svg.innerHTML = "";
+    svg.setAttribute("width", 1280);
+    svg.setAttribute("height", 650);
+    svg.setAttribute("viewBox", "0 0 1280 650");
     var normalizedFrequencies = getNormalizedFrequencies(frequencies, 600);
-    canvas.fillStyle = "#aaa";
     for (let i = 0; i < frequencies.length; i++)
-        canvas.fillRect(i * 5, 600 - normalizedFrequencies[i], 5, normalizedFrequencies[i])
-    canvas.textAlign = "center";
-    canvas.textBaseline = "middle";
-    canvas.fillStyle = "#000";
-    canvas.font = "28px Inter";
-    canvas.fillText("← Approximately "+ cardinality + " total elements →", 640, 625);
+        createRectangle(svg, i * 5, 600 - normalizedFrequencies[i], 5, normalizedFrequencies[i]);
+    const txt = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    txt.setAttribute("x", 640);
+    txt.setAttribute("y", 625);
+    txt.setAttribute("alignment-baseline", "middle");
+    txt.setAttribute("text-anchor", "middle");
+    txt.setAttribute("font-size", "28");
+    txt.setAttribute("fill", "#000");
+    txt.textContent = "← Approximately "+ cardinality + " total elements →";
+    svg.appendChild(txt);
+    container.appendChild(svg);
 }
 
 function getNormalizedFrequencies(frequencies, maxValue = 1280) {
